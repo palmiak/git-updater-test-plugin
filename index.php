07d230175af3aaa721b7f0aa885020eff054e1b8
Maciej Palmowski <m.palmowski@freshpixels.pl>
2021-10-17 15:57:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,3 +7,12 @@ Author:      Andy & Maciek
 License:     GPL2 etc
 License URI: http://link to your plugin license
 */
+
+add_action( 'admin_notices', 'test_print_notice', 0, 0 );
+
+function test_print_notice() {
+  printf(
+		'<div class="notice notice-error"><p>%1$s</p></div>',
+		'test'
+	);
+}
