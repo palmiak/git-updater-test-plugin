6d229cdf1bcc5bdd09e0da2c8551f03c5107c907
Maciej Palmowski <m.palmowski@freshpixels.pl>
2021-10-17 18:31:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 /*
 Plugin Name: Example plugin
 Description: We will demonstrate how to make it git-updater compatible
-Version:     1.1
+Version:     1.2
 Author:      Andy & Maciek
 License:     GPL2 etc
+GitHub Plugin URI: palmiak/git-updater-test-plugin
+Primary Branch: main
+Release Asset: true
 */
 
 add_action( 'admin_notices', 'test_print_notice', 0, 0 );
@@ -12,6 +15,6 @@ add_action( 'admin_notices', 'test_print_notice', 0, 0 );
 function test_print_notice() {
 	printf(
 		'<div class="notice notice-error"><p>%1$s</p></div>',
-		'test 2'
+		'test 3'
 	);
 }
