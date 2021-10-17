c8752228a031b4f5bb5d61df24fc76385d70cfa1
Maciej Palmowski <m.palmowski@freshpixels.pl>
2021-10-17 15:51:07 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Example plugin
+Description: We will demonstrate how to make it git-updater compatible
+Version:     1.0
+Author:      Andy & Maciek
+License:     GPL2 etc
+License URI: http://link to your plugin license
+*/
